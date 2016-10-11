199499c7bad54abd38bc85278ce4029deaed25cb
olovch <vgvoleg@mail.ru>
2016-10-11 23:47:17 +0300

    1st lab

--- lab1/main.c
+++ lab1/main.c
@@ -1,7 +1,9 @@
+// Copyright 2016 Ovcharuk Oleg
 #include "stdio.h"
 #include "stdlib.h"
 #include "mpi.h"
 
+// Функция для считывания вектора из файла
 int* readVectorFromFile(char* filename, int* n){
     int k = 0, i;
     int s;
@@ -9,19 +11,19 @@ int* readVectorFromFile(char* filename, int* n){
     file = fopen(filename, "r");
     if (file == NULL){
         printf("File not found! \n");
-        return 0;
+        exit(-47);
+        return NULL;
     } else {
         while ((fscanf(file, "%d",&s)!=EOF))
-        {    if(!file) break;    //чтобы не делал лишнего
+        {    if(!file) break;
             k+=1;
         }
-        int *c = (int*) malloc(k*sizeof(int));  //должен быть динамическим
+        int *c = (int*) malloc(k*sizeof(int));
         *n = k;
-        rewind(file);    //перематываем файл для повторного чтения
+        rewind(file);
         for(i=0;i<k;i++)
         {
             fscanf(file, "%d",&c[i]);
-            printf("c[%d]=%d  ",i,c[i]);
         }
         printf("\n");
         fclose(file);
@@ -30,29 +32,83 @@ int* readVectorFromFile(char* filename, int* n){
     }
 }
 
+/* Суть задачи - нахождение наиболее близких
+ * элементов вектора. Появляется небольшая
+ * трудность в виде того, что для всех
+ * процессов нужно найти и минимальную разницу
+ * между элементами, и индекс элемента,
+ * который, образуя пару со следующим
+ * и определяет наиболее близкую пару.
+ * На помощь приходит MPI_Reduce с параметром
+ * MPI_MINLOC и MPI_2INT, специальная структура
+ * для которого описана ниже
+ **/
+typedef struct {
+    int minValue;
+    int minIndex;
+} minPair;
+
+
 int main(int argc, char **argv) {
+    int N;
+    int* vector = (int *)malloc(sizeof(int)*100000000);
+    int ROOT = 0;
+
+    // Минимальная пара, которая будет найдена в каждом процессе
+    minPair localPairVector;
+
+    // Минимальная пара из всех мининимальных пар каждого процесса (че?)
+    minPair globalPairVector;
+
+    double startWtime = 0.0, endWtime = 0.0;
+
     MPI_Init(&argc, &argv);
 
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    char filename[] = "../input.txt";
-    int vectorSize;
-    int *vector = readVectorFromFile(filename, &vectorSize);
+    // Считываем вектор из файла только в одном (нулевом) процессе
+    if (rank == ROOT) {
+        char filename[] = "../smallinput.txt";
+        vector = readVectorFromFile(filename, &N);
+    }
 
-    int minDiff = 1000000;
-    int iMin = -1;
+    // Делимся этим вектором со всеми
+    MPI_Bcast(&N, 1, MPI_INT, ROOT, MPI_COMM_WORLD);
+    MPI_Bcast(vector, N, MPI_INT, ROOT, MPI_COMM_WORLD);
 
-    int i;
-    for (i = 0; i< (vectorSize-1); i++){
-        int diff = abs(vector[i] - vector[i+1]);
-        if (diff < minDiff){
+    // Засекаем время
+    startWtime = MPI_Wtime();
+
+    // Находим ближайшую пару в поле видимости процесса
+    int minDiff = 1000000000;
+    int iMin;
+    int diff;
+
+    for (int i = rank; i < N - 1; i+=size) {
+        diff = abs(vector[i] - vector[i + 1]);
+        if (diff < minDiff) {
             minDiff = diff;
             iMin = i;
         }
     }
+    localPairVector.minValue = minDiff;
+    localPairVector.minIndex = iMin;
 
-    printf("Min: %i %i", vector[iMin], vector[iMin + 1]);
+    // Находим ближайшую из всех, найденных каждым процессом
+    MPI_Reduce(&localPairVector, &globalPairVector, 1, MPI_2INT, MPI_MINLOC, ROOT, MPI_COMM_WORLD);
+
+    // Собственно выводим результат
+    if (rank == ROOT){
+        printf("Target elements -> [%d] [%d] \n",
+               vector[globalPairVector.minIndex], vector[globalPairVector.minIndex+1]);
+
+        endWtime = MPI_Wtime();
+        printf("Time = %.15f\n", endWtime - startWtime);
+
+    }
+
+    MPI_Finalize();
     return 0;
 }
